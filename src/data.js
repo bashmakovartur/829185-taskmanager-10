29a0c6bdd0fc367754d578c1394bad414d4acda8
loosq <ipsa87@gmail.com>
2019-09-01 23:32:12 +0300

    classes start

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 import {getRandDate, getRandIntBetween, getRandBool} from "./helpers";
+import moment from "moment";
 
 const card = {
   descriptions: [`Изучить теорию`, `Сделать домашку`, `Пройти интенсив на соточку`],
@@ -65,6 +66,91 @@ const cards = (sample, count) => {
   return arr;
 };
 
+//  start 4-1
+
+class Task {
+  constructor({description, dueDate, tags, color, repeatingDays}) {
+    this._description = description;
+    this._dueDate = new Date(dueDate);
+    this._tags = tags;
+    this._color = color;
+    this._element = null;
+    this._repeatingDays = repeatingDays;
+  }
+
+  getTemplate() {
+    return `<article class="card card--${this._color} ${Object.values(this._repeatingDays).some(it => it === true) ? `card--repeat` : ``}">
+            <div class="card__form">
+              <div class="card__inner">
+                <div class="card__control">
+                  <button type="button" class="card__btn card__btn--edit">
+                    edit
+                  </button>
+                  <button type="button" class="card__btn card__btn--archive">
+                    archive
+                  </button>
+                  <button
+                    type="button"
+                    class="card__btn card__btn--favorites card__btn--disabled"
+                  >
+                    favorites
+                  </button>
+                </div>
+
+                <div class="card__color-bar">
+                  <svg class="card__color-bar-wave" width="100%" height="10">
+                    <use xlink:href="#wave"></use>
+                  </svg>
+                </div>
+
+                <div class="card__textarea-wrap">
+                  <p class="card__text">${this._description}</p>
+                </div>
+
+                <div class="card__settings">
+                  <div class="card__details">
+                    <div class="card__dates">
+                      <div class="card__date-deadline">
+                        <p class="card__input-deadline-wrap">
+                          <span class="card__date">${this._dueDate.toDateString()}</span>
+                          <span class="card__time">${this._dueDate.getHours()}:${this._dueDate.getMinutes()}</span>
+                        </p>
+                      </div>
+                    </div>
+
+                    <div class="card__hashtag">
+                      <div class="card__hashtag-list">
+                        ${(Array.from(this._tags).map(tag => (`
+                          <span class="card__hashtag-inner">
+                          <input type="hidden" name="hashtag" value="${tag}" class="card__hashtag-hidden-input" />
+                          <button type="button" class="card__hashtag-name">#${tag}</button>
+                          <button type="button" class="card__hashtag-delete">delete</button>
+                        </span>`.trim()))).join(``)}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </article>`;
+  }
+
+}
+
+class TaskEdit {
+  constructor({description, dueDate, tags, color, repeatingDays}) {
+    this._description = description;
+    this._dueDate = new Date(dueDate);
+    this._tags = tags;
+    this._colors = color;
+    this._element = null;
+    this._repeatingDays = repeatingDays;
+  }
+}
+
+const TOTAL_TASKS = 3;
+
+//  end 4-1
 const TOTAL_CARDS = 14;
 export const tasks = cards(card, TOTAL_CARDS);
 
